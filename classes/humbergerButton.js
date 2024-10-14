fdcdcc8a2e24f6b06990c97cd8c48dfb8f40de0c
Jeovany-Afana <wilfrieddylan451@gmail.com>
2024-10-14 00:46:19 +0000

    Dernier commit

--- classes/humbergerButton.js
+++ classes/humbergerButton.js
@@ -4,7 +4,7 @@
   const toggler = document.getElementById('navbar-toggler');
   const navbarCollapse = document.getElementById('navbar-collapse');
 
-  console.log(toggler)
+
   toggler.addEventListener('click', function() {
     navbarCollapse.classList.toggle('active');
   });
